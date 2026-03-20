d63e59f55d1fc8416cb4932b17869ed1b50ba568
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2026-03-19 17:52:53 -0700

    allow filtering history

--- src/spottings/history.rs
+++ src/spottings/history.rs
@@ -1,10 +1,11 @@
 use std::num::NonZeroUsize;
 
-use anyhow::Context as _;
+use anyhow::{Context as _, bail, ensure};
 use entity::{spotting_message, spotting_victim};
 use itertools::Itertools as _;
-use sea_orm::{EntityTrait as _, QueryOrder as _};
-use serenity::all::{Mentionable as _, UserId};
+use poise::ChoiceParameter;
+use sea_orm::{ColumnTrait, Condition, EntityTrait as _, QueryFilter, QueryOrder as _};
+use serenity::all::{Mentionable as _, User, UserId};
 
 use crate::{
     AppContext, AppError,
@@ -14,14 +15,67 @@ use crate::{
     },
 };
 
+#[derive(ChoiceParameter, PartialEq, Eq, Copy, Clone, Debug, Hash)]
+enum HistoryFilterType {
+    #[name = "Socials"]
+    Social,
+    #[name = "Snipes"]
+    Snipe,
+}
+
 /// View the history of past snipes
 #[poise::command(prefix_command, slash_command, guild_only)]
-pub(crate) async fn history(ctx: AppContext<'_>) -> Result<(), AppError> {
+pub(crate) async fn history(
+    ctx: AppContext<'_>,
+    #[description = "Type of spotting to filter by"] 
+    mut r#type: Option<HistoryFilterType>,
+    #[description = "User who is present in either a snipe or social"]
+    participant: Option<User>,
+    #[description = "(snipes only) the person taking the snipe"] 
+    sniper: Option<User>,
+    #[description = "(snipes only) the person being sniped"] 
+    snipe_victim: Option<User>,
+) -> Result<(), AppError> {
+    let has_snipe_field = sniper.is_some() || snipe_victim.is_some();
+
+    if has_snipe_field {
+        ensure!(participant.is_none(), "`participant` cannot be used with `sniper` or `snipe_victim`");
+        // automatically set the filter type if possible
+        r#type = match r#type {
+            Some(HistoryFilterType::Social) => bail!("`sniper` or `snipe_victim` is for snipes only"),
+            _ => Some(HistoryFilterType::Snipe),
+        };
+    }
+
     let conn = &ctx.data().db;
+
+    let filter_cond = [
+        r#type.map(|t: HistoryFilterType|
+            Condition::all()
+                .add(spotting_message::Column::IsSocial.eq(matches!(t, HistoryFilterType::Social)))
+        ),
+        participant.map(|user| {
+            Condition::any()
+                .add(spotting_message::Column::AuthorId.eq(user.id.get()))
+                .add(spotting_victim::Column::VictimId.eq(user.id.get()))
+        }),
+        sniper.map(|user| {
+            Condition::all()
+                .add(spotting_message::Column::AuthorId.eq(user.id.get()))
+        }),
+        snipe_victim.map(|user| {
+            Condition::all()
+                .add(spotting_victim::Column::VictimId.eq(user.id.get()))
+        })
+    ]
+    .into_iter()
+    .flatten()
+    .fold(Condition::all(), |acc, curr| acc.add(curr));
 
     let got = spotting_message::Entity::find()
         .order_by_desc(spotting_message::Column::MessageId)
         .find_with_related(spotting_victim::Entity)
+        .filter(filter_cond)
         .all(conn)
         .await
         .context("log get recent snipes")?;
